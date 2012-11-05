a08382a2e920caf7b7ff354e4af370f2cdcb1e28
Chris Griego <cgriego@gmail.com>
2012-11-05 16:27:29 -0600

    Only install xtrabackup in xtrabackup recipe

--- recipes/percona_xtrabackup.rb
+++ recipes/percona_xtrabackup.rb
@@ -1,15 +1,2 @@
-include_recipe 'apt'
-
+include_recipe "mysql::percona_repo"
 package "percona-xtrabackup"
-
-directory "/mnt/xtrabackup" do
-  owner "root"
-  group "root"
-  mode 0755
-end
-
-cron "removing previous xtrabackup" do
-  hour "23"
-  minute "1"
-  command "rm -rf /mnt/xtrabackup/mysql && /usr/bin/innobackupex /mnt/xtrabackup/mysql --no-timestamp && /usr/bin/innobackupex --apply-log /mnt/xtrabackup/mysql && /usr/bin/innobackupex --apply-log /mnt/xtrabackup/mysql"
-end
