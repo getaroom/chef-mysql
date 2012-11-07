f218a06fec6b487f7c1ad6df8bcef65e4730f5d4
Chris Griego <cgriego@gmail.com>
2012-11-06 20:50:33 -0600

    Switch to installing innotop from tarball

---
+++ attributes/innotop.rb
@@ -0,0 +1,2 @@
+default['mysql']['innotop']['version'] = "1.9.0"
+default['mysql']['innotop']['checksum'] = "bc2669ea88fb728c6c4ed9c695c4d0b7b9ae5df0f158ef23e8853694bf4f240a"
--- recipes/innotop.rb
+++ recipes/innotop.rb
@@ -1,2 +1,17 @@
-include_recipe "mysql::client"
-package "libterm-readkey-perl"
+remote_file "#{Chef::Config['file_cache_path']}/innotop-#{node['mysql']['innotop']['version']}.tar.gz" do
+  source "http://innotop.googlecode.com/files/innotop-#{node['mysql']['innotop']['version']}.tar.gz"
+  checksum node['mysql']['innotop']['checksum']
+  backup false
+  notifies :run, "execute[install innotop]", :immediately
+end
+
+execute "install innotop" do
+  action :nothing
+  command [
+    "tar zxvf innotop-#{node['mysql']['innotop']['version']}.tar.gz",
+    "cd innotop-#{node['mysql']['innotop']['version']}",
+    "perl Makefile.PL",
+    "make install",
+  ].join(" && ")
+  cwd Chef::Config['file_cache_path']
+end
